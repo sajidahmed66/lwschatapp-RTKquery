01b057384768f78f1cce4c11a37db2f82bd52c7f
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-09-12 20:05:00 +0600

    init

---
+++ src/components/inbox/chatbody/ChatHead.tsx
@@ -0,0 +1,17 @@
+export interface IChatHeadProps {
+  avatar: string;
+  name: string;
+}
+
+export default function ChatHead({ avatar, name }: IChatHeadProps) {
+  return (
+    <div className="relative flex items-center p-3 border-b border-gray-300">
+      <img
+        className="object-cover w-10 h-10 rounded-full"
+        src={avatar}
+        alt={name}
+      />
+      <span className="block ml-2 font-bold text-gray-600">{name}</span>
+    </div>
+  );
+}
