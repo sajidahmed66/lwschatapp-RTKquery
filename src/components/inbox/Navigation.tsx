a56414e032e1bb0dc24f936ea699556fc0fef003
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-09-14 20:28:01 +0600

    logout implementation

--- src/components/inbox/Navigation.tsx
+++ src/components/inbox/Navigation.tsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
 import logoImage from "../../assets/images/lws-logo-dark.svg";
-
+import { userLoggedOut } from "../../features/auth/authSlice";
+import { useAppDispatch } from "../../app/hooks";
 export default function Navigation() {
+  const dispatch = useAppDispatch();
+
+  const logOut = () => {
+    dispatch(userLoggedOut());
+    localStorage.removeItem("auth");
+  };
+
   return (
-    <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
-      <div className="max-w-7xl mx-auto">
-        <div className="flex justify-between h-16 items-center">
+    <nav className="sticky top-0 z-40 transition-colors border-b border-general bg-violet-700">
+      <div className="mx-auto max-w-7xl">
+        <div className="flex items-center justify-between h-16">
           <Link to="/">
             <img className="h-10" src={logoImage} alt="Learn with Sumit" />
           </Link>
           <ul>
-            <li className="text-white">
-              <a href="#">Logout</a>
+            <li
+              className="text-white"
+              onClick={() => {
+                logOut();
+              }}
+            >
+              <span className="cursor-pointer">Logout</span>
             </li>
           </ul>
         </div>
